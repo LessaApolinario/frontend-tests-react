0f474fd196cfe088247f19314282e8766b548814
Lessa Apolinario <lessaapolinario@gmail.com>
2025-07-21 14:42:06 -0300

    Add login page, login validation and tsconfig paths

---
+++ src/ui/frontend/pages/login/index.tsx
@@ -0,0 +1,87 @@
+import { useRef, useState } from "react";
+import { BiSolidHide, BiSolidShow } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { EmailOrPasswordInvalidError } from "../../../../core/errors/EmailOrPasswordInvalidError";
+
+export function LoginPage() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+  const navigate = useNavigate();
+
+  function handleLogin(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (
+      emailRef.current?.value === "admin@email.com" &&
+      passwordRef.current?.value === "123456"
+    ) {
+      navigate("/dashboard");
+    } else {
+      throw new EmailOrPasswordInvalidError();
+    }
+  }
+
+  function togglePasswordVisibility() {
+    if (isPasswordHidden) {
+      setIsPasswordHidden(false);
+    } else {
+      setIsPasswordHidden(true);
+    }
+  }
+
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-800">
+      <form
+        className="flex flex-col justify-center gap-2 rounded-lg bg-white p-16"
+        onSubmit={handleLogin}
+      >
+        <h2 className="mb-3.5 w-full text-center text-2xl text-blue-800">
+          Realizar login
+        </h2>
+
+        <label htmlFor="email-input w-full">
+          <div className="text-lg text-blue-800">Email</div>
+
+          <input
+            ref={emailRef}
+            id="email-input"
+            type="text"
+            className="text-md w-full rounded-lg border-2 border-blue-800 p-2 text-blue-800 outline-blue-800"
+          />
+        </label>
+
+        <label htmlFor="password-input w-full">
+          <div className="text-lg text-blue-800">Senha</div>
+
+          <div className="flex items-center justify-center gap-1.5">
+            <input
+              ref={passwordRef}
+              id="password-input"
+              className="text-md w-full rounded-lg border-2 border-blue-800 p-2 text-blue-800 outline-blue-800"
+              type={isPasswordHidden ? "password" : "text"}
+            />
+            {isPasswordHidden ? (
+              <BiSolidShow
+                onClick={togglePasswordVisibility}
+                className="cursor-pointer text-3xl text-blue-800"
+              />
+            ) : (
+              <BiSolidHide
+                onClick={togglePasswordVisibility}
+                className="cursor-pointer text-3xl text-blue-800"
+              />
+            )}
+          </div>
+        </label>
+
+        <button
+          type="submit"
+          className="w-full cursor-pointer rounded-lg bg-blue-800 p-2 text-center text-lg font-bold text-white"
+        >
+          Entrar
+        </button>
+      </form>
+    </main>
+  );
+}
--- src/ui/frontend/routes/router.tsx
+++ src/ui/frontend/routes/router.tsx
@@ -1,18 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { MainLayout } from '../layouts/MainLayout'
-import { HomePage } from '../pages/home'
+import { MainLayout } from "@/layouts/MainLayout";
+import { HomePage } from "@/pages/home";
+import { LoginPage } from "@/pages/login";
+import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       {
-        path: '/',
+        path: "/",
+        element: <LoginPage />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/dashboard",
         element: <HomePage />,
       },
     ],
   },
-])
+]);
 
-export { router }
+export { router };
